5cfbed476c1948b42b03ac6ec3bcdd6f16676dda
Willian Marvzn <willmr13@gmail.com>
2024-04-09 12:13:27 -0300

    fix: enabled cors and helmet

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
       saveUninitialized: false,
     }),
   );
+  app.use(helmet());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
